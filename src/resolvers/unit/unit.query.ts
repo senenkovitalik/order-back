77d28334eea9f2442e977be7a75b773d748e6728
Vitaliy Senenko <vitaliy.senenko@gmail.com>
2026-02-15 20:56:18 +0200

    feat: Add unit resolver

--- src/resolvers/unit/unit.query.ts
+++ src/resolvers/unit/unit.query.ts
@@ -1,8 +1,15 @@
-import { MutationResolvers } from "../../graphql.js";
-import { prisma } from "../../lib/prismaClient.js";
+import { QueryResolvers } from '../../graphql.js'
+import { prisma } from '../../lib/prismaClient.js'
 
-export const unitQuery: MutationResolvers = {
+export const unitQuery: QueryResolvers = {
   units: async () => {
-    return await prisma.unit.findMany();
+    return await prisma.unit.findMany()
   },
-};
+  unit: async (_, { id }) => {
+    return await prisma.unit.findUnique({
+      where: {
+        id,
+      },
+    })
+  },
+}
