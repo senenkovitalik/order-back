cbbb4657bedf840b1bd2fc078f675a6a917a29b1
Vitaliy Senenko <vitaliy.senenko@gmail.com>
2026-02-15 20:09:56 +0200

    fix: Add field resolution

--- src/resolvers/employee/employee.query.ts
+++ src/resolvers/employee/employee.query.ts
@@ -3,18 +3,11 @@ import { prisma } from '../../lib/prismaClient.js'
 
 export const employeeQuery: QueryResolvers = {
   employees: async () => {
-    return await prisma.employee.findMany({
-      include: {
-        unit: true,
-      },
-    })
+    return await prisma.employee.findMany()
   },
   employee: async (_, { id }) => {
     return await prisma.employee.findUnique({
       where: { id },
-      include: {
-        unit: true,
-      },
     })
   },
 }
--- src/resolvers/employee/employee.resolver.ts
+++ src/resolvers/employee/employee.resolver.ts
@@ -1,7 +1,16 @@
 import { employeeQuery } from "./employee.query.js";
 import { employeeMutation } from "./employee.mutation.js";
+import { EmployeeResolvers } from "../../graphql.js";
+import { prisma } from "../../lib/prismaClient.js";
+
+const employeeFieldResolver: EmployeeResolvers = {
+  unit: async (parent) => {
+    return await prisma.unit.findUnique({ where: { id: parent.unitId } })
+  },
+}
 
 export const employeeResolvers = {
   Query: employeeQuery,
   Mutation: employeeMutation,
+  Employee: employeeFieldResolver,
 };
